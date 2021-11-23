a07492068d7e8af63ccd1676f87eb1550a9908a9
Dimitris Mavroudis <im.dimitris.mavroudis@gmail.com>
2021-11-24 00:06:40 +0200

    Update dependencies

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -39,7 +39,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-	imports: [RouterModule.forRoot(routes, {scrollPositionRestoration: 'top'})],
+	imports: [RouterModule.forRoot(routes, { scrollPositionRestoration: 'top', relativeLinkResolution: 'legacy' })],
 	exports: [RouterModule],
 	providers: [ProjectService, ProfileService, SkillsService]
 })
